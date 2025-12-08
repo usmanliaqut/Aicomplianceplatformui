72c7cea94f8351d46e6ad0026b266ea0333594ed
hamdtrailfive <hamdullah@trailfive.com>
2025-12-08 21:58:21 +0500

    minor fix

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -3,13 +3,13 @@ import api from "./api";
 import { Project } from "../types/project";
 
 export const getProjects = async (): Promise<Project[]> => {
-  const { data } = await api.get("/projects");
+  const { data } = await api.get("/projects/get");
   return data;
 };
 
 export const createProject = async (
   project: Partial<Project>
 ): Promise<Project> => {
-  const { data } = await api.post("/projects", project);
+  const { data } = await api.post("/projects/create", project);
   return data;
 };
